929cc8e05aff164e7cc8cda1c112efb01b847e90
djai04 <danselmo@correo.um.edu.uy>
2023-06-06 22:43:24 -0300

    Empece a preparar la clasa para cargar los datos.

--- src/uy/edu/um/prog2/entities/LoadCSV.java
+++ src/uy/edu/um/prog2/entities/LoadCSV.java
@@ -3,7 +3,12 @@ package uy.edu.um.prog2.entities;
 import org.apache.commons.csv.*;
 import java.io.*;
 
+import static java.lang.System.currentTimeMillis;
+
 public class LoadCSV {
+
+    // En el constructor podriamos recorrer la totalidad del CSV, crear los objetos,
+    //  e ir insertandolos en las estructuras de datos
     public LoadCSV() throws IOException {
         Reader in = new FileReader("dataset/f1_dataset_test.csv");
         Iterable<CSVRecord> records = CSVFormat.EXCEL.parse(in);
@@ -18,5 +23,48 @@ public class LoadCSV {
                 break;
             }
         }
+    }
+
+    // Devuelve la cantidad de CSV records que no son consistentes con la cantidad de columnas
+    public static int checkDataConsistency(String csvPath) throws IOException {
+        Reader in = new FileReader(csvPath);
+        Iterable<CSVRecord> records = CSVFormat.EXCEL.parse(in);
+        int consistencyCounter = 0;
+
+        for (CSVRecord record : records) {
+            if (!record.isConsistent()) {
+                consistencyCounter++;
+            }
+        }
+
+        return consistencyCounter;
+    }
+
+    // Devuelve la cantidad de milisegundos que toma recorrer el dataset.
+    public static long checkTimeSpent(String csvPath) throws IOException {
+        Reader in = new FileReader(csvPath);
+        Iterable<CSVRecord> records = CSVFormat.EXCEL.parse(in);
+
+        long timeStart = currentTimeMillis();
+        for (CSVRecord record : records) {
+
+        }
+        long timeEnd = currentTimeMillis();
+
+        return timeEnd - timeStart;
+    }
+
+    // Devuelve la cantidad de records en el CSV.
+    public static long checkAmountOfRecords (String csvPath) throws IOException {
+        Reader in = new FileReader(csvPath);
+        Iterable<CSVRecord> records = CSVFormat.EXCEL.parse(in);
+
+        long amountOfRecords = 0;
+
+        for (CSVRecord record : records) {
+            amountOfRecords++;
+        }
+
+        return amountOfRecords - 1;
     }
 }
--- src/uy/edu/um/prog2/entities/Main.java
+++ src/uy/edu/um/prog2/entities/Main.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        LoadCSV data = new LoadCSV();
+        // LoadCSV data = new LoadCSV();
+        System.out.println(LoadCSV.checkAmountOfRecords("dataset/f1_dataset_test.csv"));
     }
 }
