662581303c582d97e0cf5ce01ec3924dd5d004af
Baltasar Carrasco <baltasar.carrasco@innovant.us>
2023-06-11 22:37:01 -0300

    test

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         System.out.println("Hola");
+        System.out.println("Chau");
     }
 
 }
