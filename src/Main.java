611e903dfc240faf0e3e0b0572c3d7dcfea1f4bf
Baltasar Carrasco <baltasar.carrasco@innovant.us>
2023-06-11 22:40:25 -0300

    test

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         System.out.println("Hola");
         System.out.println("Chau");
+        System.out.println("Chau");
     }
 
 }
