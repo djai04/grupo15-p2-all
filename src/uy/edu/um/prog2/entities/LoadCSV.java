3a1bf9b69aa7cfaa0f575e17469f3472b93a8712
Baltasar Carrasco <baltasar.carrasco@innovant.us>
2023-06-13 12:09:57 -0300

    Cambiamos la manera en la cual se manejan las filas corruptas.

--- src/uy/edu/um/prog2/entities/LoadCSV.java
+++ src/uy/edu/um/prog2/entities/LoadCSV.java
@@ -26,99 +26,95 @@ public class LoadCSV {
         Iterable<CSVRecord> records = CSVFormat.EXCEL.parse(in);
         int counter = 0;
         boolean isFirst = true;
+        int amountOfRecordsSkipped = 0;
 
         for (CSVRecord record : records) {
-            if (isFirst) {
-                isFirst = false;
-                continue;
-            }
-
-            String tweetId = record.get(0);
-
-            String userName = record.get(1);
-
-            String userFavourites = record.get(7);
-
-            String isVerified = record.get(8);
-            boolean isVerifiedBoolean;
-
-            String tweetDate = record.get(9);
-
-            String tweetText = record.get(10);
-
-            String tweetHashtags = record.get(11);
-            LinkedList<String> tweetHastagsInLL = new LinkedList<>();
-
-            String tweetSource = record.get(12);
-
-            String tweetIsRetweet = record.get(13);
-            boolean isRetweetBoolean;
-
             try {
-                Double.valueOf(userFavourites);
-            } catch (Exception e) {
-                // System.out.println("Te encontre");
-                continue;
-            }
+                if (isFirst) {
+                    isFirst = false;
+                    continue;
+                }
 
-            // Create user
-            if (isVerified == "False") {
-                isVerifiedBoolean = false;
-            } else {
-                isVerifiedBoolean = true;
-            }
+                String tweetId = record.get(0);
+
+                String userName = record.get(1);
+
+                String userFavourites = record.get(7);
+
+                String isVerified = record.get(8);
+                boolean isVerifiedBoolean;
+
+                String tweetDate = record.get(9);
+
+                String tweetText = record.get(10);
+
+                String tweetHashtags = record.get(11);
+                LinkedList<String> tweetHastagsInLL = new LinkedList<>();
+
+                String tweetSource = record.get(12);
+
+                String tweetIsRetweet = record.get(13);
+                boolean isRetweetBoolean;
+
+                // Create user
+                if (isVerified == "False") {
+                    isVerifiedBoolean = false;
+                } else {
+                    isVerifiedBoolean = true;
+                }
 
 
-            User currentUser = new User(userName, isVerifiedBoolean, Double.valueOf(userFavourites));
+                User currentUser = new User(userName, isVerifiedBoolean, Double.valueOf(userFavourites));
 
-            // Create tweet
-            //  Validate isRetweet
-            if (tweetIsRetweet == "False") {
-                isRetweetBoolean = false;
-            } else {
-                isRetweetBoolean = true;
-            }
+                // Create tweet
+                //  Validate isRetweet
+                if (tweetIsRetweet == "False") {
+                    isRetweetBoolean = false;
+                } else {
+                    isRetweetBoolean = true;
+                }
 
-            //  Validate tweetHashtags
-            //  Should be a linked list of type Hashtag
-            tweetHashtags = tweetHashtags.replace("[", "").replace("]", "").replace("'", "").replace(" ", "");
-            String[] tweetHashtagsInArray = tweetHashtags.split(",");
+                //  Validate tweetHashtags
+                //  Should be a linked list of type Hashtag
+                tweetHashtags = tweetHashtags.replace("[", "").replace("]", "").replace("'", "").replace(" ", "");
+                String[] tweetHashtagsInArray = tweetHashtags.split(",");
 
-            for (int i = 0; i < tweetHashtagsInArray.length; i++) {
-                tweetHastagsInLL.add(tweetHashtagsInArray[i]);
-            }
+                for (int i = 0; i < tweetHashtagsInArray.length; i++) {
+                    tweetHastagsInLL.add(tweetHashtagsInArray[i]);
+                }
 
-            //  Validate tweetDate
-            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-            Date tweetDateObject = null;
-            try {
+                //  Validate tweetDate
+                SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                Date tweetDateObject = null;
                 tweetDateObject = formatter.parse(tweetDate);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
 
-            Tweet currentTweet = new Tweet(Long.valueOf(tweetId), tweetText, tweetSource, isRetweetBoolean, tweetDateObject, currentUser);
-            currentTweet.setHashtags(tweetHastagsInLL);
+                Tweet currentTweet = new Tweet(Long.valueOf(tweetId), tweetText, tweetSource, isRetweetBoolean, tweetDateObject, currentUser);
+                currentTweet.setHashtags(tweetHastagsInLL);
 
-            if (allUsers.contains(currentUser.getUsername())) {
-                User foundUser = allUsers.get(currentUser.getUsername());
-                foundUser.getUserTweets().add(currentTweet);
+                if (allUsers.contains(currentUser.getUsername())) {
+                    User foundUser = allUsers.get(currentUser.getUsername());
+                    foundUser.getUserTweets().add(currentTweet);
+                    allTweets.put(currentTweet.getId(), currentTweet);
+                    counter++;
+                    continue;
+                }
+
+                currentUser.getUserTweets().add(currentTweet);
+
+                allUsers.put(currentUser.getUsername(), currentUser);
+
                 allTweets.put(currentTweet.getId(), currentTweet);
+
                 counter++;
+                if (counter == 100000) {
+                    break;
+                }
+            } catch (Exception e) {
+                amountOfRecordsSkipped++;
                 continue;
-            }
-
-            currentUser.getUserTweets().add(currentTweet);
-
-            allUsers.put(currentUser.getUsername(), currentUser);
-
-            allTweets.put(currentTweet.getId(), currentTweet);
-
-            counter++;
-            if (counter == 100000) {
-                break;
             }
         }
+        System.out.println(amountOfRecordsSkipped);
 
     }
 
